01680c561cb767bb9fd643a199f33c00447ae146
Spanos <nickspanosf1@gmail.com>
2024-12-09 17:09:58 +0200

    grid template areas

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,33 @@
 'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
+import "./flex-grid.css";
 
-import {useEffect, useState} from "react";
 
 export default function Home() {
 
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    return (
+        <div>
 
-      </main>
-    </div>
-  );
+            <div className='grid-container'>
+
+                <div className='grid-item grid-item-1'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sanctus
+                    duis dolores aliquid odio enim ea
+                    nibh sanctus cillum invidunt nihil consequat. Eos feugait ullamcorper facer accusam pariatur laoreet
+                    facer nonummy vero esse est reprehenderit ex soluta cupiditat deserunt volutpat ad facilisis enim
+                    gubergren aliqua facilisis volutpat accumsan. Tation blandit magna id deserunt sed autem voluptate.
+                    Invidunt exerci dolores qui mollit dolore sit aute sint odio ex obcaecat hendrerit consectetuer
+                    veniam vero obcaecat. Commodi fugiat aute blandit adipisici in tincidunt volutpat facilisis odio
+                    clita consetetur consequat hendrerit eros veniam ea vulputate. Illum nam anim. Eos ad labore tation.
+                    Eirmod nostrud ea.
+                </div>
+                <div className='grid-item grid-item-2'>side bar
+                </div>
+                <div className='grid-item grid-item-3'>Sit</div>
+                <div className='grid-item grid-item-4'>Amet</div>
+                <div className='grid-item grid-item-5'>Consectetur</div>
+
+            </div>
+        </div>
+    );
 }
