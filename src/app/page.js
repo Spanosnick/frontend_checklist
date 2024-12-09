624e6292abce128fbf090366bb6092d0dc72835f
Spanos <nickspanosf1@gmail.com>
2024-12-09 14:31:35 +0200

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,51 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+ const [word, setWord] = useState("hello");
+ const actions = ['INSERT_word:5','DELETE_2'];
+
+ function changeWord(action,word_input){
+    let action_split = action.split('_');
+    let action_type = action_split[0];
+    let action_value = action_split[1];
+     let new_word = '';
+    if (action_type === 'INSERT'){
+      let letter = action_value.split(':')[0];
+      let position = action_value.split(':')[1];
+      let part1 = word_input.slice(0,position);
+      let part2 = word_input.slice(position);
+       new_word = part1 + letter + part2;
+      return new_word;
+    }else if (action_type === 'DELETE') {
+        console.log('1'+ word_input);
+        let position = action_value;
+        let part1 = word_input.slice(0,position);
+        let positionAfter = parseInt(position) + 1;
+        let part2 = word_input.slice(positionAfter);
+        console.log('2'+ part1);
+        console.log('3'+ part2);
+         new_word = part1 + part2;
+        return new_word;
+    }
+
+ }
+ useEffect( () => {
+      const newWord1 = changeWord(actions[0],word);
+      const newWord2 = changeWord(actions[1],newWord1);
+      console.log('THIS IS THE NEW WORD --> '+newWord2);
+ },[])
+
+
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        {word}
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
